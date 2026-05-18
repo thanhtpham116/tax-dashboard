77ecc429b2641d7140b54f8cb0c620c48051e1f0
Your Name <your@email.com>
2026-05-18 15:02:12 -0400

    added rrsp

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import {
 
 export default function Home() {
   const [salary, setSalary] = useState("");
+  const [rrsp, setRrsp] = useState("");
   const [results, setResults] = useState(null);
 const formatCurrency = (value) => {
   return value.toLocaleString("en-CA", {
@@ -21,22 +22,24 @@ const formatCurrency = (value) => {
 };
   const calculateTax = () => {
   const income = Number(salary);
+  const rrspContribution = Number(rrsp);
+  const taxableIncome = income - rrspContribution;
 if (!salary || income <= 0) {
   alert("Please enter a valid income.");
   return;
 }
 let federalTaxRate;
 let quebecTaxRate;
-if (income <= 50000) {
+if (taxableIncome <= 50000) {
   federalTaxRate = 0.15;
-} else if (income <= 100000) {
+} else if (taxableIncome <= 100000) {
   federalTaxRate = 0.20;
 } else {
   federalTaxRate = 0.25;
 }
-if (income <= 50000) {
+if (taxableIncome <= 50000) {
   quebecTaxRate = 0.10;
-} else if (income <= 100000) {
+} else if (taxableIncome <= 100000) {
   quebecTaxRate = 0.15;
 } else {
   quebecTaxRate = 0.20;
@@ -48,8 +51,12 @@ const quebecTax = income * quebecTaxRate;
   const totalTax = federalTax + quebecTax;
 
   const netIncome = income - totalTax;
+  const taxSavings = rrspContribution * 0.25;
 
   setResults({
+    taxableIncome,
+rrspContribution,
+taxSavings,
     federalTax,
     quebecTax,
     totalTax,
@@ -87,6 +94,13 @@ const quebecTax = income * quebecTaxRate;
         onChange={(e) => setSalary(e.target.value)}
         className="border border-gray-300 p-3 rounded-lg w-full text-lg"
       />
+      <input
+  type="number"
+  placeholder="Enter RRSP contribution"
+  value={rrsp}
+  onChange={(e) => setRrsp(e.target.value)}
+  className="border border-gray-300 p-3 rounded-lg w-full text-lg"
+/>
 
       <button
         onClick={calculateTax}
@@ -98,6 +112,15 @@ const quebecTax = income * quebecTaxRate;
 {results && (
   <div className="bg-gray-50 rounded-xl p-4 flex flex-col gap-3 text-lg">
     <p className="flex justify-between">
+      <p className="flex justify-between">
+  <span>RRSP Contribution</span>
+  <span>{formatCurrency(results.rrspContribution)}</span>
+</p>
+
+<p className="flex justify-between">
+  <span>Taxable Income</span>
+  <span>{formatCurrency(results.taxableIncome)}</span>
+</p>
   <span>Federal Tax</span>
   <span>${results.federalTax.toFixed(2)}</span>
  {formatCurrency(results.federalTax)}</p>
@@ -111,6 +134,10 @@ const quebecTax = income * quebecTaxRate;
   <span>Total Tax</span>
   <span>${results.totalTax.toFixed(2)}</span>
  {formatCurrency(results.totalTax)}</p>
+ <p className="flex justify-between font-bold text-blue-600">
+  <span>Estimated Tax Savings</span>
+  <span>{formatCurrency(results.taxSavings)}</span>
+</p>
 
     <p className="flex justify-between font-bold text-green-600">
   <span>Net Income</span>
