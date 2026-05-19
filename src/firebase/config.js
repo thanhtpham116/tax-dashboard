2fb6b725366b5a3c9a1a19e3a6f1eb65444c258d
Your Name <your@email.com>
2026-05-19 02:09:36 -0400

    added firestore db export

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCfel3qg_MqKR0Sg4t-1hOxFaNwm9jPrWA",
   authDomain: "tax-dashboard-db8ae.firebaseapp.com",
@@ -12,5 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
+export const db = getFirestore(app);
